Fixed issue with incorrect unit conversion in calculation of magnetic moment per unit volume of Co
Added histogram to output of simulate-nv-spectrum-from-cuboid.py

Added python code for generating histogram for M=(1,0,0), M=(0,1,0), &
M = (0,0,1) configurations

magnetic_field_outside_uniformly_magnetized_cuboid.py
@@ -69,15 +69,12 @@ def magneticFieldCalc(x1,x2,y1,y2,z1,z2,\
     #Avogadro's Constant: N = 6.022e23 atoms/mol
     #Magnetic moment of volume of Co: [(mu * N * rho) / (MM)] * dV
     mu = 1.71 * (9.274*(10**(-24))) # J / (T atom)
-    rho = 8900. # kg / m3
+    rho = 8900000. # kg / m3
     molmass = 58.9332 # g / mol
     Navo = 6.022*(10**23) # atoms / mol
     M = (mu * Navo * rho) / molmass
     Marray = np.array([Mx,My,Mz])
     Marray = (Marray/np.linalg.norm(Marray)) * M
-##    Marray = np.array([0,0,M])
-##    Marray = np.array([0,M/np.sqrt(2),M/np.sqrt(2)])
-##    Marray = np.array([M/np.sqrt(3),M/np.sqrt(3),M/np.sqrt(3)])
     dVx = xstep
     dVy = ystep
     dVz = zstep

simulate-nv-spectrum-from-cuboid.py
@@ -12,7 +12,7 @@ magneticFieldCalc(.0011,.0019,.0011,.0019,.002,.0028,\
                   .0014,.0016,.0014,.0016,.0014,.0016,\
                   5,5,5,\
                   5,5,5,\
-                  0,1,1)
+                  0,1,0)
 
 #Upload Bavg data from magneticFieldCalc
 Bxavg, Byavg, Bzavg, Bxstd, Bystd, Bzstd = \
@@ -37,8 +37,8 @@ simulatedEV = eigenvalues(zfArray, BxyzArray)
 zeroB = np.array([0,0,0,0,0,0])
 zfEV = eigenvalues(zfArray, zeroB)
 #Print eigenvalues for reference
-print 'ZF: ' + str(zfEV)
-print 'simulated: ' + str(simulatedEV)
+print 'ZF: ' + str(zfEV) + '\n'
+print 'simulated: ' + str(simulatedEV) + '\n'
 print (simulatedEV[6] - simulatedEV[1])/2
 print (zfEV[6] - zfEV[1])/2
 
@@ -46,7 +46,7 @@ print (zfEV[6] - zfEV[1])/2
 
 #Generate spectra
 freq = np.arange(2.77e9,2.97e9,1e6)
-ampArray = np.array([1,1,1,1,1,1,1,1])
+ampArray = np.array([1e7,1e7,1e7,1e7,1e7,1e7,1e7,1e7])
 simulatedSpectra = lor8(freq,zfArray,ampArray,simulatedEV)
 zfSpectra = lor8(freq,zfArray,ampArray,zfEV)
 
@@ -58,11 +58,30 @@ vectorfield = np.loadtxt('vectorfield.txt', delimiter = ', ', unpack=False)
 #Next comes the actual plotting
 try:
     x,y,z,Bx,By,Bz = zip(*vectorfield)
-    fig = plt.figure(figsize=plt.figaspect(1.))    
-    ax = fig.add_subplot(211,projection='3d')
+    fig = plt.figure(figsize=plt.figaspect(1.))
+    
+    ax = fig.add_subplot(321,projection='3d')
     ax.quiver(x,y,z,Bx,By,Bz,pivot='middle',length=.2,normalize=False)
-    ax = fig.add_subplot(212)
+    
+    ax = fig.add_subplot(323)
     ax.plot(freq,zfSpectra,'b-',freq,simulatedSpectra,'r-')
+    ax.annotate('D = ..., E = ...', xy=(2.88e9,2))
+    
+    ax = fig.add_subplot(322)
+    ax = plt.hist(vectorfield[:,3],bins=100,density=False)
+    ax = plt.xlim(-np.amax(np.absolute(vectorfield[:,3])),\
+                           np.amax(np.absolute(vectorfield[:,3])))
+    
+    ax = fig.add_subplot(324)
+    ax = plt.hist(vectorfield[:,4],bins=100)
+    ax = plt.xlim(-np.amax(np.absolute(vectorfield[:,4])),\
+                           np.amax(np.absolute(vectorfield[:,4])))
+    
+    ax = fig.add_subplot(326)
+    ax = plt.hist(vectorfield[:,5],bins=100)
+    ax = plt.xlim(-np.amax(np.absolute(vectorfield[:,5])),\
+                           np.amax(np.absolute(vectorfield[:,5])))
+    
     plt.draw()
     plt.show()
 except KeyboardInterrupt: